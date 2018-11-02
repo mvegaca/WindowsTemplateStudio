ab648818999d892e1623f15b7846ca856666b7e6
Martin Vega <mvegaca@outlook.com>
2018-11-02 11:17:51 +0100

    Add ContentGrid Templates for C# CodeBehind

---
+++ templates/Uwp/Pages/ContentGrid.CodeBehind/Views/ContentGridViewPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Param_ItemNamespace.Models;
+using Param_ItemNamespace.Services;
+
+namespace Param_ItemNamespace.Views
+{
+    public sealed partial class ContentGridViewPage : Page, System.ComponentModel.INotifyPropertyChanged
+    {
+        public ObservableCollection<SampleOrder> Source
+        {
+            get
+            {
+                // TODO WTS: Replace this with your actual data
+                return SampleDataService.GetContentGridData();
+            }
+        }
+
+        public ContentGridViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem is SampleOrder item)
+            {
+                NavigationService.Navigate<ContentGridViewDetailPage>(item);
+            }
+        }
+    }
+}
