d6ce7d2b709b208150122569cbe17301a7268ebe
Martin Vega <mvegaca@outlook.com>
2018-11-02 11:17:38 +0100

    Add ContentGrid Templates for C# MVVMBasic and MVVMLight

---
+++ templates/Uwp/Pages/ContentGrid/ViewModels/ContentGridViewViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Param_ItemNamespace.Models;
+using Param_ItemNamespace.Services;
+
+namespace Param_ItemNamespace.ViewModels
+{
+    public class ContentGridViewViewModel : System.ComponentModel.INotifyPropertyChanged
+    {
+        private ICommand _itemClickCommand;        
+
+        public ICommand ItemClickCommand => _itemClickCommand ?? (_itemClickCommand = new RelayCommand<SampleOrder>(OnItemClick));
+
+        public ObservableCollection<SampleOrder> Source
+        {
+            get
+            {
+                // TODO WTS: Replace this with your actual data
+                return SampleDataService.GetContentGridData();
+            }
+        }
+
+        public ContentGridViewViewModel()
+        {
+        }
+
+        private void OnItemClick(SampleOrder clickedItem)
+        {
+            if (clickedItem != null)
+            {
+            }
+        }
+    }
+}
---
+++ templates/Uwp/_composition/_shared/Page.ContentGrid.SampleData/Services/SampleDataService$wts.ItemName_gpostaction.cs
@@ -0,0 +1,21 @@
+﻿//{**
+// This code block adds the method `GetContentGridData()` to the SampleDataService of your project.
+//**}
+//{[{
+using System.Threading.Tasks;
+//}]}
+namespace Param_ItemNamespace.Services
+{
+    public static class SampleDataService
+    {
+//^^
+//{[{
+
+        // TODO WTS: Remove this once your ContentGrid page is displaying real data
+        public static ObservableCollection<SampleOrder> GetContentGridData()
+        {
+            return new ObservableCollection<SampleOrder>(AllOrders());
+        }
+//}]}
+    }
+}
