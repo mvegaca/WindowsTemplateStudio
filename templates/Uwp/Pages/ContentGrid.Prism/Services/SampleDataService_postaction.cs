98a236f7f7401df95ffd98cb0236629ac1c67b78
Martin Vega <mvegaca@outlook.com>
2018-11-02 11:18:02 +0100

    Add ContentGrid Templates for C# Prism

---
+++ templates/Uwp/Pages/ContentGrid.Prism/Services/SampleDataService_postaction.cs
@@ -0,0 +1,20 @@
+﻿//{**
+// This code block adds the method `GetContentGridData()` to the SampleDataService of your project.
+//**}
+//{[{
+using System.Threading.Tasks;
+//}]}
+namespace Param_ItemNamespace.Services
+{
+    public class SampleDataService : ISampleDataService
+    {
+//^^
+//{[{
+        // TODO WTS: Remove this once your ContentGrid page is displaying real data
+        public ObservableCollection<SampleOrder> GetContentGridData()
+        {
+            return new ObservableCollection<SampleOrder>(AllOrders());
+        }
+//}]}
+    }
+}
